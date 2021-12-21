ba7e4cebfe01877aab0b22e8aa0d019aed13103f
git504 <git504@mailo.com>
2021-12-21 17:09:15 +0100

    li filter

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log("%c index.js", "color: green; font-weight:bold;");
 
 import { renderRecipes } from "./JS/api.js";
 import * as cards from "./JS/displayCards.js";
-import * as utils from "./JS/utils.js";
 import * as filters from "./JS/displayFilters.js";
+import * as utils from "./JS/utils.js";
 
 // RECUPERE LA DATA ET HYDRATE LES COMPOSANTS
 export const GET_RECIPES_HYDRATE = (renderRecipes.prototype.getAllRecipes =
