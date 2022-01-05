e7090871ce8af2f8aa73d31ca464aa9ba06b3c56
git504 <git504@mailo.com>
2022-01-05 10:50:20 +0100

    console.time(index.js);

--- JS/api.js
+++ JS/api.js
@@ -24,31 +24,3 @@ export function renderRecipes(data) {
     return data;
   };
 }
-
-// DISPLAY BTN ONCLIKCK
-let button = document.querySelectorAll(".filter__select").forEach;
-let buttonValue = button.getAttribute("value");
-console.log(buttonValue);
-
-const changeInputTypeInText = (button, buttonValue) => {
-  button.setAttribute("type", "text");
-  button.setAttribute("data-value", `${buttonValue}`);
-  button.removeAttribute("value");
-  // rotatearrow : filter__custom-arrow filter__custom-arrow--rotate
-  // filter__custom-select : style : width 50%
-  // filter__custom-menu ajouter filter__show
-  switch (buttonValue) {
-    case "Appareil":
-      button.setAttribute("placeholder", "Recherche un appareil");
-      break;
-    case "Ingrédients":
-      button.setAttribute("placeholder", "Recherche un ingrédient");
-      break;
-    case "Ustensiles":
-      button.setAttribute("placeholder", "Recherche un ustensile");
-      break;
-    default:
-      break;
-  }
-};
-//changeInputTypeInText(button, buttonValue);
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("%c index.js", "color: green; font-weight:bold;");
+console.time("index.js");
 ("use strict");
 
 import { renderRecipes } from "./JS/api.js";
@@ -14,6 +14,41 @@ export const GET_RECIPES_HYDRATE = (renderRecipes.prototype.getAllRecipes =
     filters.DISPLAY_FILTERS(recipes);
   });
 
-  
 // AFFICHE LES TAGS  ET UPDATE
 tags.showListOfTags(tags.tagsArray);
+
+// // DISPLAY BTN ONCLIKCK
+let buttons = document.querySelectorAll(".filter__select");
+let buttonValue;
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    buttonValue = btn.getAttribute("value");
+    console.log(buttonValue, btn);
+    //changeInputTypeInText(btn, buttonValue);
+  });
+});
+
+const changeInputTypeInText = (button, buttonValue) => {
+  button.setAttribute("type", "text");
+  button.setAttribute("data-value", `${buttonValue}`);
+  button.removeAttribute("value");
+  // rotatearrow : filter__custom-arrow filter__custom-arrow--rotate
+  // filter__custom-select : style : width 50%
+  // filter__custom-menu ajouter filter__show
+  switch (buttonValue) {
+    case "Appareil":
+      button.setAttribute("placeholder", "Recherche un appareil");
+      break;
+    case "Ingrédients":
+      button.setAttribute("placeholder", "Recherche un ingrédient");
+      break;
+    case "Ustensiles":
+      button.setAttribute("placeholder", "Recherche un ustensile");
+      break;
+    default:
+      break;
+  }
+};
+
+console.timeEnd("index.js");
